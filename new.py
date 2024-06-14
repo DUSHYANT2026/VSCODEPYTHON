9760bb568f10d365b8ac1d300efbe1e6e6c24329
Dushyant2026 <dushyantpandit799@gmail.com>
2024-06-14 21:42:15 +0530

    save the changes

--- new.py
+++ new.py
@@ -1,9 +1,9 @@
-# n = int(input())
+n = int(input())
 
-# for i in range (0 , n):
-#     for j in range (0 , i):
-#         print('*',end="")
-#     print()
+for i in range (1 , n+1):
+    for j in range (1 , i+1):
+        print('*',end="")
+    print()
         
 # print("king of kings, the kings")
         
