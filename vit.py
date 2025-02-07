9c31bc99e25bb8a30a54feb8f3965b64581711a7
Dushyant <dushyantpandit799@gmail.com>
2025-02-07 11:00:50 +0530

    mark all the changes

---
+++ vit.py
@@ -0,0 +1,48 @@
+# code to reverse the string
+
+# s = input('ENTER THE STRING ')
+# news = s[::-1]
+# print('The reverse of the enter string ',news)
+
+
+
+
+# code to check the string is the palindrome
+
+# s = input('ENTER THE STRING ')
+# news = s[::-1]
+# if(s == news):
+#     print('Yes the string is palindrome ',news)
+# else:
+#     print('NO the string is palindrome ',news)
+    
+    
+    
+# find the count of upper and lower char in a string
+
+# s = input('ENTER THE STRING ')
+# lower = 0  
+# upper = 0
+# space = 0
+# for i in s:
+#     if(i.islower()):
+#         lower = lower + 1;
+#     elif(i.isupper()):
+#         upper = upper + 1;
+#     else:
+#         space = space + 1;
+# print('The number of upper char in string is ', upper)
+# print('The number of lower char in string is' ,lower)
+# print('The number of space in string is' ,space)
+
+
+# code to check the string  is panagram or not
+
+# s = input('ENTER THE STRING ')
+# nums = set()
+# for it in s:
+#     nums.add(it)
+# if(len(nums) == 26):
+#     print('The string is Panagram ',s)
+# else:
+#     print('The string is not Panagram', s)
